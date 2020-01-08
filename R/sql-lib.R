12826c2ba3cf4d9abb4d178d08a251dcb81b2e56
alejandro baranek <alejandrobaranek@gmail.com>
2020-01-08 15:24:23 -0300

    0.1.1

--- R/sql-lib.R
+++ R/sql-lib.R
@@ -12,7 +12,30 @@
 #'
 #' @description
 #' This class is intended to simplify SQL commands.
+#' @examples
+#' db.name <- getMtcarsdbPath(copy = TRUE)
+#' rsql <- createRSQL(drv = RSQLite::SQLite(), dbname = db.name)
+#' where_values_df <- data.frame(carb = 8, stringsAsFactors = FALSE)
+#' select_sql <- rsql$gen_select(
+#'  select_fields = "*", #c("wt", "qsec"),
+#'  table = "mtcars",
+#'  where_values = where_values_df)
+#' mtcars.observed <- rsql$execute_select(select_sql)
+#'  mtcars.observed
 #'
+#'  mtcars.new <- mtcars.observed
+#' mtcars.new$carb <- 9
+#' insert_sql <- rsql$gen_insert(table = "mtcars", values_df = mtcars.new)
+#' rsql$execute_insert(sql_insert = insert_sql)
+#'
+#' where_values_df <- data.frame(carb = 9, stringsAsFactors = FALSE)
+#' select_sql <- rsql$gen_select(
+#'   select_fields = "*", #c("wt", "qsec"),
+#'  table = "mtcars",
+#'  where_values = where_values_df)
+#' mtcars.observed <- rsql$execute_select(select_sql)
+#' mtcars.observed
+#' å
 #' @export
 #' @importFrom R6 R6Class
 RSQL.class <- R6::R6Class("RSQL", public = list(
